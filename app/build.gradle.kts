323caf40c6ee8d278b1b8fed067c64d772ae4443
alireza <seyed.alireza.nejati.80@gmail.com>
2024-05-21 18:34:55 +0330

    release v1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.alirezasn80.learn_en"
         minSdk = 21
         targetSdk = 34
-        versionCode = 121
-        versionName = "1.2.1"
+        versionCode = 122
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
