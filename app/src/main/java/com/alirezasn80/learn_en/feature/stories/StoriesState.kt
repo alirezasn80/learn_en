1a04f831c9327aee6a1b57b7cf0cb317a618cc46
alireza <seyed.alireza.nejati.80@gmail.com>
2024-06-01 18:24:52 +0330

    work on local

--- app/src/main/java/com/alirezasn80/learn_en/feature/stories/StoriesState.kt
+++ app/src/main/java/com/alirezasn80/learn_en/feature/stories/StoriesState.kt
@@ -1,15 +1,11 @@
 package com.alirezasn80.learn_en.feature.stories
 
-import com.alirezasn80.learn_en.core.domain.entity.Items
+
 import com.alirezasn80.learn_en.feature.stories.model.Book
-import com.alirezasn80.learn_en.utill.Progress
 
 data class StoriesState(
     val title: String = "",
-    val items: List<Items> = emptyList(),
     val isLastReadStory: Int = 0,
-
-
     val page: Int = 1,
     val totalPage: Int = 1,
     val tickets: List<Book> = emptyList(),
