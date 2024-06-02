60ef3e034b3ec59c2a8e5ef5fef1fa38a2020349
alireza <seyed.alireza.nejati.80@gmail.com>
2024-06-02 11:54:26 +0330

    changes

--- app/src/main/java/com/alirezasn80/learn_en/feature/stories/StoriesViewModel.kt
+++ app/src/main/java/com/alirezasn80/learn_en/feature/stories/StoriesViewModel.kt
@@ -6,28 +6,14 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.alirezasn80.learn_en.core.data.database.AppDB
 import com.alirezasn80.learn_en.core.data.datastore.AppDataStore
-import com.alirezasn80.learn_en.core.data.service.ApiService
-import com.alirezasn80.learn_en.core.domain.entity.toItems
 import com.alirezasn80.learn_en.utill.Arg
 import com.alirezasn80.learn_en.utill.BaseViewModel
 import com.alirezasn80.learn_en.utill.Key
-import com.alirezasn80.learn_en.utill.debug
 import com.alirezasn80.learn_en.utill.getString
-import com.alirezasn80.learn_en.utill.toPart
-import com.alirezasn80.learn_en.utill.toRB
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
-import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Part
-import retrofit2.http.Path
 import java.io.File
 import javax.inject.Inject
 
@@ -37,7 +23,6 @@ class StoriesViewModel @Inject constructor(
     savedstate: SavedStateHandle,
     private val database: AppDB,
     private val dataStore: AppDataStore,
-    private val apiService: ApiService,
     private val application: Application,
 ) : BaseViewModel<StoriesState>(StoriesState()) {
     val categoryId = savedstate.getString(Arg.CATEGORY_ID)!!.toInt()
@@ -48,28 +33,8 @@ class StoriesViewModel @Inject constructor(
         state.update { it.copy(title = title) }
         getStories()
         getLastReadCategory()
-        // addBook()
     }
 
-    private fun addBook() {
-
-        viewModelScope.launch(Dispatchers.IO) {
-            val story = database.contentDao.getStoriesByCategory(categoryId).first()//todo()
-            try {
-                val result = apiService.addBook(
-                    categoryId = "4".toRB(),
-                    name = story.title.toRB(),
-                    cover = null,
-                    file = createTXT(application, story.title, story.content).toPart("file")
-                )
-                debug(result)
-            } catch (e: Exception) {
-                debug("Error!")
-            }
-
-
-        }
-    }
 
     private fun createTXT(context: Context, title: String, body: String): File {
         val file = File(context.cacheDir, title)
