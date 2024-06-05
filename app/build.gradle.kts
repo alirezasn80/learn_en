4c4467ac74388f005e5a9021db71877e57ad67df
alireza <seyed.alireza.nejati.80@gmail.com>
2024-06-05 12:45:15 +0330

    Fix bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.alirezasn80.learn_en"
         minSdk = 21
         targetSdk = 34
-        versionCode = 123
-        versionName = "1.2.3"
+        versionCode = 130
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
